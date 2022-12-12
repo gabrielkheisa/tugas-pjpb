28423c5c2cdcd5bdf17f3aa8340d26f9f9329d9d
gapradana123 <115344560+gapradana123@users.noreply.github.com>
2022-12-12 17:25:27 +0700

    Delete list.php

--- list.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-    $connection = new mysqli("localhost","root","","validator");
-    $data       = mysqli_query($connection, "select * from profil");
-    $data       = mysqli_fetch_all($data, MYSQLI_ASSOC);
-
-    echo json_encode($data);
